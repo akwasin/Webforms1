9fe49ff50200d7222bdbd40bc9f139fb4ead6f63
akwasin <akwasiijunior@gmail.com>
2016-02-03 21:43:19 +0100

    Work in progress

---
+++ Webforms1/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Webforms1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="input" runat="server"></asp:TextBox>
+
+        <asp:DropDownList ID="dd1" runat="server">
+            <asp:ListItem Value="">(Select)</asp:ListItem>
+            <asp:ListItem>mm</asp:ListItem>
+            <asp:ListItem>cm</asp:ListItem>
+            <asp:ListItem>dm</asp:ListItem>
+            <asp:ListItem>m</asp:ListItem>
+            <asp:ListItem>km</asp:ListItem>
+            <asp:ListItem>mil</asp:ListItem>
+        </asp:DropDownList>
+        
+        <asp:Label ID="toLabel" runat="server">to</asp:Label>
+
+        <asp:DropDownList ID="dd2" runat="server">
+            <asp:ListItem Value="">(Select)</asp:ListItem>
+            <asp:ListItem>mm</asp:ListItem>
+            <asp:ListItem>cm</asp:ListItem>
+            <asp:ListItem>dm</asp:ListItem>
+            <asp:ListItem>m</asp:ListItem>
+            <asp:ListItem>km</asp:ListItem>
+            <asp:ListItem>mil</asp:ListItem>
+        </asp:DropDownList>
+        
+        <asp:Button runat="server" id="ConvertButton" Text="Convert This" OnClick="ConvertButton_Click"/>
+
+        <asp:Label ID="output" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Webforms1/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webforms1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //dd2.SelectedIndexChanged +=
+                //new EventHandler(OutputUnit_SelectedValueChanged);
+        }
+
+        //private void OutputUnit_SelectedValueChanged(object sender, EventArgs e)
+        private void ConverterMethod()
+        {
+            string inputVal = input.Text;
+            string inputUnit = dd1.SelectedValue;
+            string outputUnit = dd2.SelectedValue;
+            string outputText = output.Text;
+            double numberVal;
+            double result;
+
+            if (inputUnit.Equals(outputUnit))
+            {
+                outputText = "Same Value entered";
+            }
+            else if (inputUnit == "mm" && outputUnit == "cm")
+            {
+                double.TryParse(inputVal, out numberVal);
+                result = numberVal * 0.1;
+                outputText = result.ToString();
+            }
+        }
+
+        protected void ConvertButton_Click(object sender, EventArgs e)
+        {
+            string inputVal = input.Text;
+            string inputUnit = dd1.SelectedValue;
+            string outputUnit = dd2.SelectedValue;
+            string outputText = output.Text;
+            double numberVal;
+            double result;
+
+            if (dd1.SelectedItem.Text == "mm" && dd2.SelectedItem.Text == "mm")
+            {
+                output.Text = "Same Value entered";
+            }
+            else if (inputUnit == "mm" && outputUnit == "cm")
+            {
+                double.TryParse(inputVal, out numberVal);
+                result = numberVal * 0.1;
+                output.Text = $"{result}{outputUnit}";
+            }
+        }
+    }
+}
